4682c26f493779dc4bd41ede3fca9b9e35697e8f
Rhiju Das <rhiju@stanford.edu>
2025-02-07 16:47:18 -0800

    fix verbose in merge_reads & speed up df readin a little by only getting two cols

--- merge_reads.py
+++ merge_reads.py
@@ -17,29 +17,34 @@ parser = argparse.ArgumentParser(
 
 parser.add_argument('-i','--reads_files',nargs='*', required=True, help='names of reads.txt files to merge')
 parser.add_argument('-o','--out_tag',default='',help='name of output reads.txt file')
-parser.add_argument('-v','--verbose',default='',help='output as each sequence is processed')
+parser.add_argument('-v','--verbose',action="store_true",help='output as each sequence is processed')
 
 args = parser.parse_args()
 
 df_all = []
 if True: # later will process files that have same name but across a bunch of subdirectories, like ubr_merge.py
 
-    reads_files = args.reads_files
-    if len(reads_files)==1:
-        reads_files=sorted(glob.glob(reads_files[0]))
+    reads_files = []
+    for read_file in args.reads_files: reads_files += sorted(glob.glob(read_file))
+
     out_tag = args.out_tag.replace('.reads.txt','')
 
-    for reads_file in reads_files:
+    if args.verbose: print("Collecting information from index.csv files")
+    df0 = None
+    for (count,reads_file) in enumerate(reads_files):
         assert( reads_file.find('.reads.txt')>-1)
         index_file = reads_file.replace('.reads.txt','.index.csv')
-        df_all.append( pd.read_csv( index_file ) )
-    num_ref = len(df_all[0])
+        if args.verbose: print( 'Doing file %s, %d out of %d files' % (index_file,count+1,len(reads_files)) )
+        df_all.append( pd.read_csv( index_file, usecols=['num_reads','ref_idx'] ) )
+        if count==0: df0 = pd.read_csv( index_file)
+
+    num_ref = len(df0)
     for df in df_all: assert( len(df) == num_ref )
 
     tmp_dir = tempfile.mkdtemp(dir="/tmp")  # Creates a unique directory in /tmp
-    print(f"Temporary directory created: {tmp_dir}")
+    if args.verbose: print(f"Temporary directory created: {tmp_dir}")
     tmp_files = []
-    for ref_idx in df_all[0]['ref_idx']: # cleanup old files
+    for ref_idx in df0['ref_idx']: # cleanup old files
         tmp_file = '%s/%d.reads.txt' % (tmp_dir,ref_idx)
         tmp_files.append(tmp_file)
 
@@ -47,11 +52,11 @@ if True: # later will process files that have same name but across a bunch of su
         if os.path.isfile(tmp_file): os.remove( tmp_file )
 
     # Now go through each input reads file, and output blocks that correspond to each reference sequence.
-    for (reads_file,df) in zip(reads_files, df_all):
+    for (count,(reads_file,df)) in enumerate(zip(reads_files, df_all)):
         fid_in = open(reads_file)
         num_reads_all=df['num_reads']
         num_reads_total=0
-        if args.verbose: print( 'Doing file %s, %d out of %d files' % (reads_file,count,len(read_files)
+        if args.verbose: print( 'Doing file %s, %d out of %d files' % (reads_file,count+1,len(reads_files)) )
         for (ref_idx,num_reads,tmp_file) in zip(df['ref_idx'],df['num_reads'],tmp_files):
             fid_out = open( tmp_file, 'a')
             for n in range(num_reads): fid_out.write( fid_in.readline() )
@@ -62,30 +67,33 @@ if True: # later will process files that have same name but across a bunch of su
         fid_out.close()
         fid_in.close()
 
-    df_out=df_all[0]
+    df_out=df0
     read_start = 1
+    if args.verbose: print('Getting ready to output',index_file)
     for n in range(num_ref):
         num_reads = 0
+        if args.verbose and (n+1) % 1000 == 0: print( 'Figuring out total reads for ref sequence %d out of %d' % (n+1, num_ref) )
         for df in df_all: num_reads += df.iloc[n]['num_reads']
         df_out.at[n,'read_start'] = read_start
         df_out.at[n,'read_end']   = read_start + num_reads - 1
         df_out.at[n,'num_reads']  = num_reads
         read_start += num_reads
     index_file=out_tag+'.index.csv'
-    df.to_csv( index_file, index=False )
+    df_out.to_csv( index_file, index=False )
     print('Outputted %d reads for %d sequences to %s' % (read_start-1,num_ref,index_file) )
-
 
     # Now concatenate files above.
     fid_out = open(out_tag+'.reads.txt','w')
-    for (tmp_file,num_reads) in zip(tmp_files,df_out['num_reads']):
+    for (count,(tmp_file,num_reads)) in enumerate(zip(tmp_files,df_out['num_reads'])):
         fid_in = open( tmp_file )
+        if args.verbose and (count+1) % 1000 == 0: print( 'Concatenating %s, file %d out of %d' % (tmp_file, count+1, len(tmp_files)) )
         for n in range( num_reads): fid_out.write( fid_in.readline() )
         assert( not fid_in.readline() ) # hit end of file?
         fid_in.close()
     fid_out.close()
 
     # get rid of tmp files.
+    if args.verbose: print('Deleting',tmp_dir)
     shutil.rmtree(tmp_dir)
 
 
